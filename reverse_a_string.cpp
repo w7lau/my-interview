053f876bb0f3549878c53a70878e370fff6dc287
w7lau <33317926+w7lau@users.noreply.github.com>
2017-11-24 21:48:49 +0800

    my first Interview
    My first interview is in my school,the interviewer give me a question,how to reverse a string? I can use string,or character array,I chose the character,but I think my code is not good,so when I came back to my dormitory,and I do it again,and decide to save it. I just a beginner to learn C++, so ,maybe my code look not so good.

---
+++ reverse_a_string.cpp
@@ -0,0 +1,44 @@
+﻿#include "stdafx.h"
+#include<cstring>
+#include<string>
+#include<cctype>
+#include<stdlib.h>
+
+void change(char* ch);
+using namespace std;
+int main()
+{
+	int i = 0;
+	string str;
+	cin >> str;
+	size_t len = strlen(str.c_str());
+	char *ch = new char[len + 1];		
+	strncpy(ch, str.c_str(), len + 1);/*get the string lenth and copy it to the char array*/
+	change(ch);
+	cout << ch << endl;
+	//char ch1[] = "ploikujyhtgrfedwsqa";
+	//change(ch1);
+	//cout << ch1 << endl;
+	while (1);
+    return 0;
+}
+
+void change(char* ch)
+{
+	int k=0;
+	while (1)
+	{
+		if (ch[k] == '\0')
+			break;
+		k++;/*find the max number*/
+	}
+	for ( int i = 0; i < (k/2); i++)/*find the middle one*/
+	{
+		char temp;
+		temp = ch[i];
+		ch[i] = ch[k-1 - i];
+		ch[k-1 - i] = temp;
+
+	}
+	ch[k] = '\0';
+}
